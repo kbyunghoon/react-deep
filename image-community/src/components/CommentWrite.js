5258bd97275a45b0dbe1be92a55036859ab01bca
kbyunghoon <79817983+kbyunghoon@users.noreply.github.com>
2021-03-30 15:21:13 +0900

    recent

--- image-community/src/components/CommentWrite.js
+++ image-community/src/components/CommentWrite.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import { Grid, Input, Button } from "../elements"
+import { actionCreators as commentActions } from "../redux/modules/comment"
 
 const CommentWrite = (props) => {
+    const dispatch = useDispatch();
+    const [comment_text, setComment_text] = useState();
+
+
+    const { post_id } = props;
+    const onChange = (e) => {
+        setComment_text(e.target.value)
+    }
+
+    const write = () => {
+        console.log(comment_text)
+        dispatch(commentActions.addCommentFB(post_id, comment_text));
+        setComment_text("");
+    }
 
     return (
         <React.Fragment>
             <Grid padding="16px" is_flex>
-                <Input placeholder={"댓글 내용을 입력해주세요 :)"} />
-                <Button width="50px" margin="0px 2px 0px 2px">작성</Button>
+                <Input placeholder={"댓글 내용을 입력해주세요 :)"} _onChange={onChange} value={comment_text} onSubmit={write} is_submit />
+                <Button width="50px" margin="0px 2px 0px 2px" _onClick={write}>작성</Button>
             </Grid>
         </React.Fragment>
     )
